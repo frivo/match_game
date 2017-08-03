7edc51cf1d667ad2d9bc2bc7021f3c132ab0f565
frivo <frivo@users.noreply.github.com>
2017-08-03 18:15:59 +0200

    add js function 'generateCardValues'

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -10,6 +10,24 @@ var MatchGame = {};
  */
 
 MatchGame.generateCardValues = function () {
+	var numbers = [];
+
+	for (var i = 1; i <= 8; i++) {
+		numbers.push(i);
+		numbers.push(i);
+	}
+
+	function shuffleArray(array) {
+	    for (var i = array.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	    }
+	    return array;
+	};
+
+	return numbers = shuffleArray(numbers);
 
 };
 
